14f29a6a6b4af771e3512653d63bc6b74896ba0b
meicookies <79348524+meicookies@users.noreply.github.com>
2021-03-24 18:11:11 +0700

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -9,11 +9,11 @@ echo -ne "Input list: "
 read web
 echo "Wait....."
 cat "$web" | sed 's/https:\/\///g;s/http:\/\///g;s/www.//g' > 3
-list='damn'
+list='3'
 while IFS= read -r l
 do
 kk=$(curl -sL http://jamet1337.ml/api/subdo.php?url=$l | head -4 | tail -1 | sed 's/hasil//g;s/"//g;s/://g;s/,//g;s/\\r\\n/ \n/g;s/Author Jamet1337//g;s/ //g;s/www.//g;s/cpanel.//g;s/cpcontacts.//g;s/cpcalendars.//g;s/autodiscover.//g;s/mail.//g;s/webmail.//g')
 echo "$kk" | sed '/^$/d' | awk -v RS="[ \n]+" '!n[$0]++' >> result.tmp
 cat result.tmp
 done < "$list"
-rm damn
+rm 3
