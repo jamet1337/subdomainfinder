15bbf62b1a72a231e76dad6c55be78ce9af1ba2a
meicookies <79348524+meicookies@users.noreply.github.com>
2021-03-26 18:13:22 +0700

    Update same.sh

--- same.sh
+++ same.sh
@@ -5,6 +5,7 @@ if [ -z $1 ]; then
   echo "Usage: ./same.sh <domain>"
   exit 1
 fi
-curl -sL http://jamet1337.ml/api/same-domain.php?url=$1 | head -5 | tail -1 | sed '/^$/d' | awk -v RS="[ \n]+" '!n[$0]++' | sed 's/hasil//g;s/"//g;s/://g;s/,//g;s/\\r\\n/ \n/g'
+curl -sL http://jamet1337.ml/api/same-domain.php?url=$1 | head -5 | tail -1 | sed '/^$/d' | awk -v RS="[ \n]+" '!n[$0]++' | sed 's/hasil//g;s/"//g;s/://g;s/,//g;s/\\r\\n/ \n/g' > rest
+cat rest
 
 
